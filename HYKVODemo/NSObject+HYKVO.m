83bffa2b11b16c856c51a3f9b7f60f5267b3189f
张鸿运 <zhanghongyun01@58ganji.com>
2017-07-16 20:43:40 +0800

    DEV：添加实现原理

--- HYKVODemo/NSObject+HYKVO.m
+++ HYKVODemo/NSObject+HYKVO.m
@@ -14,7 +14,7 @@
 
 - (void)HY_addObserver:(NSObject *)observer forKeyPath:(NSString *)keyPath options:(NSKeyValueObservingOptions)options context:(nullable void *)context
 {
-    //1.动态创建一个监听对象的子类
+    //1 动态创建一个监听对象的子类
     //1.1 动态类名
     NSString *oldClassName = NSStringFromClass([self class]);
     NSString *newClass = [NSString stringWithFormat:@"HY_%@",oldClassName];
@@ -28,24 +28,27 @@
     //1.5 修改被观察者的isa指针,让他指向自定义的类
     object_setClass(self, myClass);
     
-    //动态绑定属性
+    //1.6 动态绑定属性
     objc_setAssociatedObject(self, (__bridge const void *)@"123", observer, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
 void setName(id self, SEL _cmd, NSString *newName)
 {
-    //保存当前类型
+    //2 执行监听对象的监听属性的修改
+    //2.1 保存当前类型 此时 self 指被监听对象的子类
     id class = [self class];
-    //改变当前对象指向父类
+    //2.2 改变当前对象指向父类 此句之后 self 为被监听的类
     object_setClass(self, class_getSuperclass([self class]));
-    //调用父类的方法
+    //2.3 调用父类的方法 此句执行了对被监听对象的监听属性进行赋值操作
     objc_msgSend(self, @selector(setName:),newName);
-    NSLog(@"修改完毕!!!");
-    //拿出观察者
+    
+    //3 赋值完毕，通知外界
+    //3.1 拿出观察者
     id obersrver = objc_getAssociatedObject(self,(__bridge const void *)@"123");
-    //通知外界
+    //3.2 通知外界
     objc_msgSend(obersrver, @selector(observeValueForKeyPath:ofObject:change:context:),self,@"name",@{@"new":newName},nil);
-    //改回子类型
+    
+    //4 改回子类型
     object_setClass(self, class);
     
 }
